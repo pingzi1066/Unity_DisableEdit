98a838a2d47d276e35a6e6220163b2cc6bfea703
WonPeace <wp2727@163.com>
2016-07-25 21:22:54 +0800

    add scene.

---
+++ UnityProject/Assets/DisableEdit/Editor/DrawerTestAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomPropertyDrawer(typeof(DisableEdit))]
+public class DrawerDisableEditAttribute : PropertyDrawer 
+{
+	public override void OnGUI (Rect position, SerializedProperty property, GUIContent label)
+	{
+		GUI.enabled = false;
+
+		EditorGUI.PropertyField (position, property);
+	}
+}
---
+++ UnityProject/Assets/TestBehaviourScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestBehaviourScript : MonoBehaviour 
+{
+    [Range(0,2)]
+    public int timeScale = 1;
+
+    [DisableEdit]
+    public float time;
+
+    [SerializeField][DisableEdit][Tooltip("time += Time.deltaTime")]
+    private string tip = "<--- Stay with 'Tip' !";
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        if (Time.timeScale != timeScale)
+        {
+            Time.timeScale = timeScale;
+        }
+
+
+    }
+}
